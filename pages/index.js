2bc9a76e4ba1d3f97ddeb27e88e4728057988433
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-02-19 22:39:26 +0000

    completed pre-rendering and data fetching modules

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    }
+  };
+}
+
+// By returning allPostsData inside the props object in getStaticProps, the blog posts will be passed to the Home component as a prop. Now you can access the blog posts like so:
+// export default function Home ({ allPostsData }) { ... }
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -12,13 +25,27 @@ export default function Home() {
         <p>
           I'm Georgia, embodying Rowlf's spirit, learning Next.JS (lucky me).
           Here's the{" "}
-          <a href="https://github.com/rockyrelay/learn-nextjs">Repo</a>for this
+          <a href="https://github.com/rockyrelay/learn-nextjs">Repo</a> for this
           site.
         </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
